788d56565bfd601f89ff7c055dc6abcb0f4d3c94
Bartosz Dyja <bartosz.dyja@gmail.com>
2015-03-11 23:17:04 +0100

    Added firstname lastname admin to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ActiveRecord::Base
+	validates :firstname, :lastname, presence: true
+	
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
   has_many :reviews
   has_many :products
+
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe User do
-  it { should validate_presence_of :firstname }
+  it { should validate_presence_of :email }
   it { should validate_presence_of :lastname }
 
   it "by default isn't admin" do
